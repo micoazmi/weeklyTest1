8226b97eb5a45f98afa2d78b250a3a6c6c36edf8
unknown <black.samurai6@gmail.com>
2024-07-05 11:29:09 +0700

    number 1 done

--- number1.go
+++ number1.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(removeDuplicateLetter("bananas"))
+	fmt.Println(removeDuplicateLetter("lalalamama"))
+
+}
+
+func removeDuplicateLetter(words string) string {
+	duplicate := ""
+	for i := 0; i < len(words); i++ {
+		found := false
+
+		for j := 0; j < len(duplicate); j++ {
+			if duplicate[j] == words[i] {
+				found = true
+				break
+			}
+		}
+		if !found {
+			duplicate += string(words[i])
+		}
+
+	}
+	return duplicate
 }
