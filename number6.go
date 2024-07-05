b7ed351354dd9673b052bf20a0f5acbfcfee3bd6
unknown <black.samurai6@gmail.com>
2024-07-05 22:25:32 +0700

    number6

---
+++ number6.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(plusOne([]int{1, 2, 3, 4}))
+	fmt.Println(plusOne([]int{1, 4, 8, 9}))
+	fmt.Println(plusOne([]int{9, 9, 9, 9}))
+
+}
+
+func plusOne(nums []int) []int {
+	result := []int{}
+	converts := 0
+	for _, num := range nums {
+		converts *= 10
+		converts += num
+	}
+	converts += 1
+
+	for converts > 0 {
+		i := converts % 10
+		result = append([]int{i}, result...)
+		i /= 10
+	}
+	return result
+}
