3342a4585e4b20c75db5c5a8b9aa1cc57b49b24c
unknown <black.samurai6@gmail.com>
2024-07-05 23:24:11 +0700

    number11&8 belum fix

---
+++ number11.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fruits1 := []string{"Mangga", "Apel", "Melon", "Pisang", "Sirsak", "Tomat", "Nanas", "Nangka", "Timun", "Mangga"}
+	fruits2 := []string{"Bayam", "Wortel", "Kangkung", "Mangga", "Tomat", "Kembang Kol", "Nangka", "Timun"}
+	fmt.Println(sliceFruits(fruits1, fruits2, 1))
+	fmt.Println(sliceFruits(fruits1, fruits2, 2))
+	fmt.Println(sliceFruits(fruits1, fruits2, 3))
+
+}
+
+func sliceFruits(fruits1 []string, fruits2 []string, operationType int) []string {
+	defaultAnswer := []string{"please input type operation between 1 or 2"}
+	result := []string{}
+	switch operationType {
+	case 1:
+		for _, fruit1 := range fruits1 {
+			for _, fruit2 := range fruits2 {
+				if fruit1 == fruit2 {
+					if !found(result, fruit2) {
+						result = append(result, fruit2)
+
+					}
+				}
+			}
+		}
+	case 2:
+		for _, fruit1 := range fruits1 {
+			for _, fruit2 := range fruits2 {
+				if fruit1 != fruit2 {
+					if !found(result, fruit1) {
+						result = append(result, fruit1)
+
+					}
+				}
+			}
+		}
+	default:
+		return defaultAnswer
+	}
+
+	return result
+
+}
+
+func found(res []string, fruit2 string) bool {
+	for _, v := range res {
+		if fruit2 == v {
+			return true
+		}
+	}
+	return false
+
+}
